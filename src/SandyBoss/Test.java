fc5eec0e489697a52dcfd3da15890fc35131787d
CodyBosss <sudhir.bk9@gmail.com>
2024-08-07 20:43:12 +0530

    reverseInteger class added

--- src/SandyBoss/Test.java
+++ src/SandyBoss/Test.java
@@ -7,27 +7,12 @@ import java.util.List;
 public class Test {
 
 	public static void main(String[] args) {
-		int nums[] = { 1,1 };
-		System.out.println(minSubsequence(nums));
-		System.out.println("Check");
+		int x = 120;
+		System.out.println(reverseInteger(x));
 	}
 
-	public static boolean minSubsequence(int[] nums) {
-		Arrays.sort(nums); // 3 4 8 9 10
-		int l = 0, r = nums.length - 1;
-		int res1 = 0, res2 = 0;
-		while (l < r - 1) {
-			res1 += nums[l];
-			l++;
-			res2 = nums[r];
-			if (res1 < res2) {
-				return true;
-			} else {
-				r--;
-				res1 -= nums[r];
-				System.out.println(res1);
-			}
-		}
-		return false;
+	public static int reverseInteger(int x) {
+		
+		return x;
 	}
 }
