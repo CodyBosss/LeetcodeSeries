2069c2659153922e9d7d5cf712f30275614c2729
SandyCoder <sudhir.bk9@gmail.com>
2024-08-08 15:02:07 +0530

    Correction completed

--- src/SandyBoss/FindIfDigitGameCanBeWon.java
+++ src/SandyBoss/FindIfDigitGameCanBeWon.java
@@ -2,24 +2,22 @@ package SandyBoss;
 
 public class FindIfDigitGameCanBeWon {
 	public static void main(String [] args) {
-		int nums [] = {1,2,3,4,5,14};
+		int nums [] = {1,2,3,4,10};
+//					= {5,5,5,25};
 		System.out.println(canAliceWin(nums));
 	}
 	public static boolean canAliceWin(int nums[]) {
 		int sum1=0, sum2=0;
 		for(int i=0; i<nums.length; i++) {
 			if(nums[i]<=9) {
-				sum1 += Math.min(sum1, nums[i]);
+				sum1 += nums[i];
 			}
 			else if(nums[i]>=10) {
-				sum2 += Math.max(sum2, nums[i]);
+				sum2 += nums[i];
 			}
 		}
 		int min = Math.min(sum1, sum2);
 		int max = Math.max(sum1, sum2);
-		if(min<max) 
-			return true;
-		
-		return false;
+		return (min<max) ? true : false;
 	}
 }
