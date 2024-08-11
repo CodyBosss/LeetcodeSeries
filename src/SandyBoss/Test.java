e88f61fed0cb8ad6cbda9ba45ab15b461d9cd38d
SandyCoder <sudhir.bk9@gmail.com>
2024-08-11 12:40:30 +0530

    check

--- src/SandyBoss/Test.java
+++ src/SandyBoss/Test.java
@@ -5,6 +5,12 @@ import java.math.BigInteger;
 public class Test {
 
 	public static void main(String[] args) {
-		
+		String str1 = "Sudhir";
+		String str2 = "Khandare";
+		System.out.println(addString(str1, str2));
+	}
+	public static String addString(String str1, String str2) {
+		String res = str1+str2;
+		return res;
 	}
 }
